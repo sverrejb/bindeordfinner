d5a8b82da8a2447042504731d80e4925f0f06e8c
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-22 23:25:12 +0100

    return valid object with empty list

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -9,9 +9,9 @@ def lambda_handler(event, context):
     second_word = body['second'].lower()
 
     if first_word == '' or second_word == '':
-        return []
-
-    solutions = find_glueword(first_word, second_word)
+        solutions = []
+    else:
+        solutions = find_glueword(first_word, second_word)
 
     return {
         'statusCode': 200,
